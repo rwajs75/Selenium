6c2400e5bfcabfdebb143bf43609cef06b475fd3
Roman Wajs <rwajs@isolution.pl>
2018-03-25 17:15:16 +0200

    zadanie 4 firefox

--- java-przyklady/src/test/java/pl/isol/LogowanieLitecart.java
+++ java-przyklady/src/test/java/pl/isol/LogowanieLitecart.java
@@ -4,15 +4,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
-
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class LogowanieLitecart {
@@ -22,8 +26,16 @@ public class LogowanieLitecart {
 
     @Before
     public void start() {
-        //driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        driver = new ChromeDriver();
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+        //caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+        //caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+        //caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        driver = new FirefoxDriver(new FirefoxOptions(caps).setLegacy(true));
+        //driver = new FirefoxDriver(new FirefoxBinary(new File("c:\\Program Files\\Nightly\\firefox.exe")), new FirefoxProfile(), caps);
+        //driver = new ChromeDriver(caps);
+        //driver = new InternetExplorerDriver(caps);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
